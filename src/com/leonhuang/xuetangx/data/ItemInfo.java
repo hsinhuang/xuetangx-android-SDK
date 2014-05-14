8440861dd43601fa7f5588f01af40b190cb73200
leon-huang <leon@njuopen.com>
2014-05-14 13:36:14 +0800

    update to latest API

--- src/com/leonhuang/xuetangx/data/ItemInfo.java
+++ src/com/leonhuang/xuetangx/data/ItemInfo.java
@@ -6,25 +6,30 @@ import org.json.JSONObject;
 public class ItemInfo extends BaseInfo {
 
 	private ItemType type;
+	private String title;
 	private String surl;
 	private VideoUrlInfo vurl;
 
-	private ItemInfo(ItemType type, String url, JSONObject json) {
+	private ItemInfo(ItemType type, String title, String url, JSONObject json) {
 		super(json);
 
+		this.title = title;
 		this.type = type;
 		this.surl = url;
 	}
 
-	private ItemInfo(ItemType type, VideoUrlInfo url, JSONObject json) {
+	private ItemInfo(ItemType type, String title, VideoUrlInfo url,
+			JSONObject json) {
 		super(json);
 
+		this.title = title;
 		this.type = type;
 		this.vurl = url;
 	}
 
 	public static ItemInfo fromJSON(JSONObject json) throws JSONException {
 		String itemType = json.getString("item_type");
+		String title = json.getString("item_title");
 		ItemType type = ItemType.VIDEO;
 		if (itemType.equals("problem")) {
 			type = ItemType.PROBLEM;
@@ -33,15 +38,19 @@ public class ItemInfo extends BaseInfo {
 		}
 
 		if (ItemType.VIDEO == type) {
-			return new ItemInfo(type, VideoUrlInfo.fromJSON(json
+			return new ItemInfo(type, title, VideoUrlInfo.fromJSON(json
 					.getJSONObject("item_url")), json);
 		} else {
-			return new ItemInfo(type, json.getString("item_url"), json);
+			return new ItemInfo(type, title, json.getString("item_url"), json);
 		}
 	}
 
 	public ItemType getType() {
 		return type;
+	}
+
+	public String getTitle() {
+		return title;
 	}
 
 	public String getProblemPageUrl() {
